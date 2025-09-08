423d80347f6446699250ff974cf60985b6af2761
Jonathan Araldi <jonathan.araldi@outlook.com>
2025-09-08 17:50:29 +0200

    feat(particles): canvas dynamique + génération aléatoire des particules
    - Dimensionnement du canvas en plein écran (selon la fenêtre).
    - Ajout de la classe Particle avec draw() et update() (rebond sur les bords).
    - fillParticulesArray() : création d’un tableau de particules (taille/position/vitesse aléatoires) en fonction de la surface.
    - randomVelocity() : génération de vitesses bidirectionnelles.
    - Stockage dans particulesArray.

--- 16.Particules/script.js
+++ 16.Particules/script.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector("#canvas-1");
 const ctx = canvas.getContext("2d")
 
-// canvas.width = window.innerWidth
-// canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 class Particle {
     constructor (x,y,directionX,directionY,size,color) {
@@ -31,3 +31,25 @@ class Particle {
     }
 }
 // console.log(new Particle(40, 10, 1, 1, 54, "#000"));
+
+let particulesArray;
+
+function fillParticulesArray() {
+    particulesArray = []
+    const particulesNumber = (canvas.height * canvas.width) / 9000
+    for (let i = 0; i < particulesNumber; i++) {
+        const size = Math.random() * 2 + 1
+        const x = Math.random() * (canvas.innerWidth - 20) + 10
+        const y = Math.random() * (canvas.innerHeight - 20) + 10
+        const directionX = randomVelocity()
+        const directionY = randomVelocity()
+        particulesArray.push(new Particle(x, y, directionX, directionY, size, "#f1f1f1"))
+    }
+}
+fillParticulesArray()
+console.log(particulesArray);
+
+function randomVelocity() {
+    const speed = Math.random() * 1 + 0.5
+    return Math.random() < 0.5 ? speed : -speed
+}
